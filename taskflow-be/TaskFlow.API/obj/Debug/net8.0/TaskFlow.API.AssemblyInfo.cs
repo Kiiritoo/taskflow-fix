152f287bc4e1a2960d2306be78001e9759502971
Kiiritoo <michael.osvaldo2008@gmail.com>
2025-04-29 13:12:33 +0700

    added few changes

--- taskflow-be/TaskFlow.API/obj/Debug/net8.0/TaskFlow.API.AssemblyInfo.cs
+++ taskflow-be/TaskFlow.API/obj/Debug/net8.0/TaskFlow.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25397d9ce8ebe3ccadd1440bbea98c70ec64a08f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
